7ae1b68d123d50c585d7974ad04aaf6d2ec26c6c
EfeErtugErdem <efeerdem20@ku.edu.tr>
2023-08-12 22:36:23 +0300

    Added up to date DGW.

--- DGW/DGW/src/main/java/com/i2icellcelly/DGW/Common/GlobalData.java
+++
@@ -1,9 +0,0 @@
-package com.i2icellcelly.DGW.Common;
-
-public class GlobalData {
-
-    public static final String HAZEL_ADDRESS = "34.133.255.110:5701";
-    public static final String MAP_NAME = "cell-celly";
-    public static final String OCS_AKKA_ADDRESS = "akka://MySystem@104.198.24.178:9090/user/listener";
-    
-}
